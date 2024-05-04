009d665ebd06b6418b97bd463ba58c4a20ca5827
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-05-04 15:06:13 -0400

    Made a loop to make the squares

---
+++ index.js
@@ -0,0 +1,11 @@
+const mainContainer = document.getElementsById("squarecnter");
+let divSize = 16;
+
+for(let i = 0; i < divSize * divSize; i++) {
+    let squareSize = (divSize * 50) / divSize;
+    const square = document.createElement("div");
+    square.classList.add("square");
+    mainContainer.appendChild(square);
+    square.style.height = `${squareSize}px`;
+    square.style.width = `${squareSize}px`;
+}
